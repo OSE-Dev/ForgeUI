14eaddf82e1a62c0a7af80cc94a9dde91cfd2147
Nick <nick.rawls@gmail.com>
2024-08-03 01:12:03 -0500

    - add gadget plop generator

--- Web/plopfile.js
+++ Web/plopfile.js
@@ -29,5 +29,36 @@ export default function (plop) {
         template: '@use \'packages/{{name}}\' as {{name}}',
       },
     ],
+  }),
+  plop.setGenerator('gadget', {
+    description: 'New Gadget',
+    prompts: [{
+      type: 'input',
+      name: 'name',
+      message: 'enter the gadget name'
+    }],
+    actions: [
+      {
+        type: 'add',
+        path: './Client/packages/gadgets/{{name}}/_{{name}}.sass',
+      },
+      {
+        type: 'addMany',
+        destination: './Client/packages/gadgets/{{name}}/tests',
+        templateFiles: 'plop_templates/components/tests/*.hbs',
+        base: 'plop_templates/components/tests'
+      },
+      {
+        type: 'addMany',
+        destination: './Client/packages/gadgets/{{name}}',
+        templateFiles: 'plop_templates/components/*.hbs',
+        base: 'plop_templates/components'
+      },
+      {
+        type: 'append',
+        path: './Client/styles/styles.sass',
+        template: '@use \'packages/{{name}}\' as {{name}}',
+      },
+    ],
   })
 };
